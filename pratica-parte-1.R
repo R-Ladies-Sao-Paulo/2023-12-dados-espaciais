b9c53166b092e28804f8ddb75e80746373bd9266
Beatriz Milz <milz.bea@gmail.com>
2023-12-10 19:57:54 -0300

    styler

--- pratica-parte-1.R
+++ pratica-parte-1.R
@@ -8,10 +8,12 @@
 link_sigbm <-
   "https://app.anm.gov.br/SIGBM/Publico/ClassificacaoNacionalDaBarragem/ExportarExcel"
 
-# Fazendo uma requisição POST neste link, 
+# Fazendo uma requisição POST neste link,
 # e salvando o arquivo localmente
-httr::POST(link_sigbm,
-           httr::write_disk("dados/sigbm.xlsx", overwrite = TRUE))
+httr::POST(
+  link_sigbm,
+  httr::write_disk("dados/sigbm.xlsx", overwrite = TRUE)
+)
 
 # Importação dos dados ------------
 
@@ -23,7 +25,7 @@ dplyr::glimpse(sigbm_bruto)
 
 # Limpeza dos dados
 
-sigbm <- sigbm_bruto |> 
+sigbm <- sigbm_bruto |>
   # Limpando nome das colunas!
   janitor::clean_names() |>
   dplyr::mutate(
@@ -35,12 +37,12 @@ sigbm <- sigbm_bruto |>
       "Nome da barragem: {nome_da_barragem} <br>
       Empreendedor: {nome_do_empreendedor}"
     )
-  ) |> 
+  ) |>
   # Removendo linhas onde lat/long é igual a 0 (erro de cadastro)
-  dplyr::filter(lat != 0, long != 0) |> 
+  dplyr::filter(lat != 0, long != 0) |>
   sf::st_as_sf(coords = c("long", "lat"), crs = 4326, remove = FALSE)
 
-# Site útil para consultar o CRS 
+# Site útil para consultar o CRS
 # https://epsg.io/
 
 # Verificando a classe
@@ -61,11 +63,11 @@ sf::st_crs(sigbm)
 
 library(ggplot2)
 
-sigbm |> 
+sigbm |>
   ggplot() +
   geom_sf()
 
-# Vamos para a visualização interativa, e depois voltamos 
+# Vamos para a visualização interativa, e depois voltamos
 
 # Visualização interativa ------------------------
 
@@ -83,42 +85,44 @@ library(leaflet)
 
 # Versão 1: mapa vazio
 
-sigbm |> 
+sigbm |>
   # Começa um mapa vazio
-  leaflet() 
+  leaflet()
 
 # Versão 2: vamos adicionar as barragens
-sigbm |> 
-  leaflet() |> 
+sigbm |>
+  leaflet() |>
   # Adiciona as barragens
   addMarkers()
 
 # Versão 3: vamos adicionar um fundo de mapa
-sigbm |> 
-  leaflet() |> 
-  addProviderTiles("Esri.WorldImagery") |> 
+sigbm |>
+  leaflet() |>
+  addProviderTiles("Esri.WorldImagery") |>
   addMarkers()
 
 # Versão 4: vamos agrupar os pontos
 
-sigbm |> 
-  leaflet() |> 
-  addProviderTiles("Esri.WorldImagery") |> 
+sigbm |>
+  leaflet() |>
+  addProviderTiles("Esri.WorldImagery") |>
   addMarkers(clusterOptions = markerClusterOptions())
 
 # Versão 5: vamos adicionar um popup
 
-sigbm |> 
-  leaflet() |> 
-  addProviderTiles("Esri.WorldImagery") |> 
-  addMarkers(clusterOptions = markerClusterOptions(),
-             popup = ~texto)
+sigbm |>
+  leaflet() |>
+  addProviderTiles("Esri.WorldImagery") |>
+  addMarkers(
+    clusterOptions = markerClusterOptions(),
+    popup = ~texto
+  )
 
 # Vamos adicionar a delimitação dos estados brasileiros?
 
 # Baixando os dados
 
-estados <- geobr::read_state() 
+estados <- geobr::read_state()
 
 
 # Voltando ao mapa: adicionando os estados!
@@ -128,8 +132,10 @@ sigbm |>
   addProviderTiles("Esri.WorldImagery") |>
   # Adicionando os estados
   addPolygons(data = estados, label = ~abbrev_state, fillOpacity = 0) |>
-  addMarkers(clusterOptions = markerClusterOptions(),
-              popup = ~ texto)
+  addMarkers(
+    clusterOptions = markerClusterOptions(),
+    popup = ~texto
+  )
 
 
 # Vamos exportar a base do sigbm já tratada
